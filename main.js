5c7a66693c562df5b40ae4e149449c4098c26282
jugal <alanjugal@gmail.com>
2021-12-05 23:03:37 +0530

    ed2

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-let library=[];
+let library=['dawdawd'];
 
-function book(name,author){
-    this.name=name
+function book(bname,author){
+    this.bname=bname
     this.author=author
     this.info=function(){
-        return name+author
+        return `${bname+author}`
     }
 }
 
-const hob=new book('awdawd','adwdaw');
+
 
 function addToLibrary(){
-    library.push(hob);    
+    library.push('daikbwdubau');    
 }
 
 addToLibrary();
 console.table(library)
 
 const card=document.querySelector('.card');
+card.innerHTML=library;
